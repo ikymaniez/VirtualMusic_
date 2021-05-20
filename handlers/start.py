ef314274914d4aa4181d473412a6e9f19e6f9064
ikymaniez <84445414+ikymaniez@users.noreply.github.com>
2021-05-20 23:34:25 +0700

    Create start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,5 +1,5 @@
 # Daisyxmusic (Telegram bot project )
-# Copyright (C) 2021  Inukaasith 
+# Copyright (C) 2021  Inukaasith
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,10 +14,9 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-from config import BOT_NAME as bn
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 
 @Client.on_message(
@@ -27,73 +26,99 @@ from config import BOT_NAME as bn
 )
 async def start_(client: Client, message: Message):
     await message.reply_text(
-        f"""<b>Haii 🖐️ {message.from_user.first_name} My Name is 𝗩𝗜𝗥𝗧𝗨𝗔𝗟 𝗠𝗨𝗦𝗜𝗖\n
-Aku Adalah Bot Music Telegram Yang Akan Menemani mu Di Voice Call Group.
-Jika Ingin Menggunakan Invite Aku Dan Asisstantnya Ke Dalam Group Lalu Angkat Bot Menjadi Admin. Jika Ada Kendala Bisa Chat Pemilik Nya.
+        f"""<b>┗┓ Haii {message.from_user.first_name} My Name is 𝙈𝙐𝙎𝙄𝘾 𝙈𝘼𝙉 ┏┛\n
+Saya Bot Music Group, Yang dapat Memutar Lagu di Voice Chat Group Dengan cara yang Mudah
+Saya Memiliki Banyak Fitur Praktis Seperti :
 ┏━━━━━━━━━━━━━━
 ┣• Memutar Musik.
 ┣• Mendownload Lagu.
 ┣• Mencari Lagu Yang ingin di Putar atau di Download.
 ┗━━━━━━━━━━━━━━
-🤵𝓒𝓻𝓮𝓪𝓽𝓮𝓭 𝓫𝔂 : [IKYY](https://t.me/boyfriendnice)
-☘️𝓣𝓱𝓪𝓷𝓴𝓼 𝓯𝓸𝓻 : [Grup Support](https://t.me/Familythunder)
-━━━━━━━━━━━━━━
-𝐁𝐎𝐓 𝐌𝐔𝐒𝐈𝐊 : @Virtualsong_bot - 𝐀𝐒𝐈𝐒𝐒𝐓𝐀𝐍𝐓 𝐌𝐔𝐒𝐈𝐊 : @AsisstantMusicVirtual
- 
+❃ Managed With ☕️ By : [Risman](https://t.me/mrismanaziz)
+━━━━━━━━━━━━━━━
+Ingin Menambahkan Saya ke Grup Anda? Tambahkan Saya Ke Group Anda!
 </b>""",
+
+# Edit Yang Perlu Lu ganti 
+# Tapi Jangan di Hapus Thanks To nya Yaaa :D
+
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
                     InlineKeyboardButton(
-                        "🤵Owner Music", url="https://t.me/boyfriendnice")
+                        "📜 Cara Menggunakan BOT 📜", url="https://t.me/Lunatic0de/20")
                   ],[
                     InlineKeyboardButton(
-                        "👥Official Group", url="https://t.me/Familythunder"
+                        "Group Support", url="https://t.me/SharingUserbot"
                     ),
                     InlineKeyboardButton(
-                        "📢Official Channel", url="https://t.me/MusikManagement") 
-                  ],[
-                    InlineKeyboardButton(
-                        "🍀Instagram", url="https://www.instagram.com/ikyyy_35"
+                        "Channel", url="https://t.me/Lunatic0de"
                     )
                 ]
             ]
         ),
-     disable_web_page_preview=True
+     disable_web_page_preview=False
     )
 
-@Client.on_message(filters.command("start") & ~filters.private & ~filters.edited)
-async def gstart(_, message: Message):
-      await message.reply_text("""**Aku sudah online, ayo kita joget ceria! 🎶**""",
-      reply_markup=InlineKeyboardMarkup(
-            [
-                [
+@Client.on_message(
+    filters.command("start")
+    & filters.group
+    & ~ filters.edited
+)
+async def start(client: Client, message: Message):
+    await message.reply_text(
+        "💁🏻‍♂️ **Apakah Anda ingin mencari Link YouTube?**",
+        reply_markup=InlineKeyboardMarkup(
+            [   
+                [    
                     InlineKeyboardButton(
-                        "🤵Owner Music", url="https://t.me/boyfriendnice"
-                    )
-                ],[
-                    InlineKeyboardButton(
-                        "✅ Yes!", switch_inline_query_current_chat=""
+                        "✅ Ya", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "❌ No!", callback_data="close"
+                        "❌ Tidak ", callback_data="close"
                     )
                 ]
             ]
         )
     )
 
-@Client.on_message(filters.command("help") & ~filters.private & ~filters.edited)
-async def gstart(_, message: Message):
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
     await message.reply_text(
-        """**Klik tombol dibawah untuk melihat panduan menggunakan bot**""",
+        """**Klik Tombol dibawah untuk Melihat Cara Menggunakan Bot**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "🦇 Cara Memakai Bot Music!", url="https://t.me/humangabutguys/91577"
+                        "📜 Cara Menggunakan BOT 📜", url="https://t.me/Lunatic0de/20"
                     )
                 ]
             ]
         ),
-    )
+    )  
+
+
+@Client.on_message(
+    filters.command("reload")
+    & filters.group
+    & ~ filters.edited
+)
+async def reload(client: Client, message: Message):
+    await message.reply_text("""✅ Bot **berhasil dimulai ulang!**\n\n• **Daftar admin** telah **diperbarui**""",
+      reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "Group Support", url="https://t.me/SharingUserbot"
+                    ),
+                    InlineKeyboardButton(
+                        "Owner", url="https://t.me/mrismanaziz"
+                    )
+                ]
+            ]
+        )
+   )
